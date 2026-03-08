311aadad719522c78b38c3e77d1be22c854e9852
anuj-shandilya <anujshandilya3@gmail.com>
2026-03-08 11:36:48 +0530

    adding seo

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+document.title = 'Padnote'
+
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
